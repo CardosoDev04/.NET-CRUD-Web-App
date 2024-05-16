ee796fd5628a8da9b68cc84a7d26f6e26bc05f97
50465 - Joao Cardoso <joaocardoson1@icloud.com>
2024-05-16 11:10:02 +0100

    Initial commit

---
+++ QuotesWebApp/Data/Migrations/20240516095525_initialsetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuotesWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quote",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuoteText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuoteAuthor = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quote", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quote");
+        }
+    }
+}
---
+++ QuotesWebApp/Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿namespace QuotesWebApp.Models
+{
+    public class Quote
+    {
+        public int ID { get; set; }
+        public required string QuoteText { get; set; }
+
+        public required string QuoteAuthor { get; set; }
+
+        public Quote()
+        {
+
+        }
+
+    }
+
+    
+}
